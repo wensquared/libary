2fb9877e1eda027e166fa7af371aeb31d1ba8dd8
wensquared <71566436+wensquared@users.noreply.github.com>
2021-08-08 16:55:01 +0200

    added todo

--- static/js/libary.js
+++ static/js/libary.js
@@ -1,2 +1,30 @@
 'use strict'
+/* 
+    TODO:
+    - btn:'Add Book' - opens a dialog window, to type in a form where fill out: author, title, number of pages and checkbox for read 
+        - dialog window: btn 'Save' - writes to localStorage with fn AddBook
+    - table: list of all the books that have been typed in + add 'Remove' 
+    - btn: 'Remove Libary' - opens dialog window - ask if user really wants to delete libary
 
+    - make class Libary
+        - constructor: 
+            check if there is a list of books - print it out - add tr to table - fn:addBook
+            when 'Add Book' btn clicked, open dialog window
+            when 'Save' btn clicked, save book - fn addBook -fn saveList
+            when 'Remove Libary' btn clicked - clear localStorage
+        - fn: addBook(form data):
+            adds book to the table and save it local with fn saveList
+            - plus add td 'Remove' (removes current tr)
+            - plus add btn to change read-status
+        - fn: saveList()
+            get the data from table and save it local
+ */
+
+class Libary {
+    constructor() {
+        let list = JSON.parse(window.localStorage.getItem('books'))
+    }
+}
+$(document).ready(function() {
+    console.log('Hello')
+})
